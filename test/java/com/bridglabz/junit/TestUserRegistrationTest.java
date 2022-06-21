3fb026db1e3f9b298126685d6bb0bf4d58a6d5b1
suraj <surajmore2143@gmail.com>
2022-06-21 17:18:28 +0530

    Added to cheack validate last name in junit feature

--- test/java/com/bridglabz/junit/TestUserRegistrationTest.java
+++ test/java/com/bridglabz/junit/TestUserRegistrationTest.java
@@ -6,9 +6,9 @@ import org.junit.Test;
 public class TestUserRegistrationTest {
 
     @Test
-    public void givenFirstName_whenProper_thenReturnTrue() {
+    public void givenLastName_whenValid_thenReturnTrue() {
         UserRegistration user = new UserRegistration();
-        boolean result = user.isFirstNameValid("Suraj");
+        boolean result = user.isLastNameValid("More");
         System.out.println(result);
         Assert.assertTrue(result);
     }
