7cfec0c926bac8eb2edf709ba810b3e8031db772
suraj <surajmore2143@gmail.com>
2022-06-22 11:13:14 +0530

    Added to cheack validate mobile no in junit feature

--- main/java/com/bridglabz/junit/UserRegistration.java
+++ main/java/com/bridglabz/junit/UserRegistration.java
@@ -3,9 +3,9 @@ package com.bridglabz.junit;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public static final String EMAIL_PATTERN = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
-    public boolean iEmailValid(String email){
-        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-        return pattern.matcher(email).matches();
+    public static final String MOBILE_NO_PATTERN = "^[0-9]{2}\\\\s[1-9]{1}[0-9]{9}$";
+    public boolean isMobileNoValid(String mobileNo){
+        Pattern pattern = Pattern.compile(MOBILE_NO_PATTERN);
+        return pattern.matcher(mobileNo).matches();
     }
 }
--- test/java/com/bridglabz/junit/TestUserRegistrationTest.java
+++ test/java/com/bridglabz/junit/TestUserRegistrationTest.java
@@ -8,7 +8,7 @@ public class TestUserRegistrationTest {
     @Test
     public void givenEmail_whenValid_thenReturnTrue() {
         UserRegistration user = new UserRegistration();
-        boolean result = user.iEmailValid("abc123@gmail.com");
+        boolean result = user.isMobileNoValid("91 9087654321");
         System.out.println(result);
         Assert.assertTrue(result);
     }
